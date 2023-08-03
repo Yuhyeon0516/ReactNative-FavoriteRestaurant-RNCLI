7926f693d866d99ba66f80741520ffd40fa11a52
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-03 19:00:09 +0900

    create getting address function

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,8 +1,9 @@
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 import React, {useCallback, useEffect, useState} from 'react';
 import {Header} from '../components/Header/Header';
 import MapView, {Marker} from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import {getAddressFromCoords} from '../utils/GeoUtils';
 
 // lat : 37.213770, long : 127.038024
 
@@ -14,14 +15,27 @@ export default function MainScreen() {
     latitude: 37.21377,
     longitude: 127.038024,
   });
+  const [currentAddress, setCurrentAddress] = useState<string | null>(null);
+
+  const onChangeLocation = useCallback<
+    (item: {latitude: number; longitude: number}) => Promise<void>
+  >(async item => {
+    setCurrentRegion({
+      latitude: item.latitude,
+      longitude: item.longitude,
+    });
+
+    getAddressFromCoords(item.latitude, item.longitude).then(setCurrentAddress);
+  }, []);
+
   const getMyLocation = useCallback(() => {
     Geolocation.getCurrentPosition(postion => {
-      setCurrentRegion({
+      onChangeLocation({
         latitude: postion.coords.latitude,
         longitude: postion.coords.longitude,
       });
     });
-  }, []);
+  }, [onChangeLocation]);
 
   useEffect(() => {
     getMyLocation();
@@ -39,6 +53,9 @@ export default function MainScreen() {
           longitude: currentRegion.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.021,
+        }}
+        onLongPress={event => {
+          onChangeLocation(event.nativeEvent.coordinate);
         }}>
         <Marker
           coordinate={{
@@ -47,6 +64,27 @@ export default function MainScreen() {
           }}
         />
       </MapView>
+      {currentAddress && (
+        <View
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 24,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              backgroundColor: 'gray',
+              paddingHorizontal: 24,
+              paddingVertical: 12,
+              borderRadius: 30,
+            }}>
+            <Text style={{fontSize: 16, color: 'white'}}>{currentAddress}</Text>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
