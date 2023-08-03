9b9be2641996ba65f7f8ba5a25988f5b578d8746
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-03 18:02:25 +0900

    connect react-native-maps

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,25 @@
 import {View} from 'react-native';
 import React from 'react';
 import {Header} from '../components/Header/Header';
+import MapView from 'react-native-maps';
+
+// lat : 37.213770, long : 127.038024
 
 export default function MainScreen() {
   return (
-    <View>
+    <View style={{flex: 1}}>
       <Header>
         <Header.Title title="Main" />
       </Header>
+      <MapView
+        style={{flex: 1}}
+        region={{
+          latitude: 37.21377,
+          longitude: 127.038024,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.021,
+        }}
+      />
     </View>
   );
 }
